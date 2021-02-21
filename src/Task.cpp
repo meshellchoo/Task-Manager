e3aeb811b6c79f61bd2c63ee5ef6adc89725ba23
meshellchoo <michellechu0131@gmail.com>
2021-02-20 16:59:45 -0800

    updated Task viewTask()

--- src/Task.cpp
+++ src/Task.cpp
@@ -2,9 +2,14 @@
 #define __TASK_CPP__
 
 #include "/header/Task.hpp" 
+#include <iostream>
+
 
 void Task::viewTask(){
-
+	std::cout << "- " << taskName << std::endl;
+	std::cout << "Type: " << taskType << std::endl;
+	std::cout << description << std::endl;
+	std::cout << "Due Date" << dueDate << std::endl;
 }
 TaskObject* Task::createTaskMemento(){
     
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "/home/csmajs/jooi001/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
-#include "/home/csmajs/jooi001/cs100-task-manager/header/Task.hpp"
-//#include "/home/csmajs/mchu017/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
-//#include "/home/csmajs/mchu017/cs100-task-manager/header/Task.hpp"
+//#include "/home/csmajs/jooi001/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
+//#include "/home/csmajs/jooi001/cs100-task-manager/header/Task.hpp"
+#include "/home/csmajs/mchu017/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
+#include "/home/csmajs/mchu017/cs100-task-manager/header/Task.hpp"
 //#include "/home/csmajs/htran164/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
 //#include "/home/csmajs/htran164/cs100-task-manager/header/Task.hpp"
 
