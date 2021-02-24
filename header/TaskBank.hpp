78179aed8e507b1f087770808150d405d407321e
galacticgoob <jameso1321@gmail.com>
2021-02-23 21:30:00 -0800

    Updated TaskBank.hpp

--- header/TaskBank.hpp
+++ header/TaskBank.hpp
@@ -1,4 +1,24 @@
 #ifndef __TASKBANK_HPP__
 #define __TASKBANK_HPP__
+#include <vector>
+#include "TaskObject.hpp"
+#include "Task.hpp"
+#include "TaskList.hpp"
+
+class TaskBank{
+
+	private:
+		std::vector<TaskObject*> schedule;
+	public:
+		~TaskBank(){};
+		void clear();
+		void display();
+		void addTask(TaskObject*);
+		void deleteTask(TaskObject*);
+		void sortByPriority();
+//		TaskObject* createTaskBankMemento();
+//		void restore(taskBankMemento);
+
+}
 
 #endif
