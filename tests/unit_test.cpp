3ebe543d54b1687a762cf444a2625497b5b15f47
ethanotran <ethanotran@gmail.com>
2021-03-08 00:08:53 -0800

    added unit test and TaskBankSortTest files

---
+++ tests/unit_test.cpp
@@ -0,0 +1,109 @@
+#ifndef _UNIT_TEST_
+#define _UNIT_TEST_
+
+#include "gtest/gtest.h"
+#include "Date.hpp"
+#include "Task.hpp"
+#include "TaskBank.hpp"
+#include "TaskCommand.hpp"
+#include "TaskList.hpp"
+#include "TaskManager.hpp"
+#include "TaskMemento.hpp"
+#include "TaskObject.hpp"
+#include "helperFunctions.hpp"
+
+TEST(Tasktest, CreateTask)
+{
+
+}
+
+TEST(TaskTest, CreateTaskList)
+{
+
+}
+
+TEST(TaskTest, setTaskName)
+{
+
+}
+
+TEST(TaskTest, setTaskDescription)
+{
+
+}
+
+TEST(TaskTest, setTaskDueDate)
+{
+}
+
+TEST(TaskTest, setTaskPriority)
+{
+
+}
+
+TEST(TaskTest, getTaskName)
+{
+}
+
+TEST(TaskTest, getTaskType)
+{
+}
+
+TEST(TaskTest, getTaskDescription)
+{
+}
+
+TEST(TaskTest, getTaskPriority)
+{
+
+}
+
+TEST(TaskTest, deleteTask)
+{
+
+}
+
+TEST(TaskTest, clearTaskList)
+{
+
+}
+
+TEST(TaskTest, SortByDueDate)
+{
+
+}
+
+TEST(TaskTest, SortByPriority)
+{
+
+}
+
+TEST(TaskTest, AddTask_TaskBank)
+{
+
+}
+
+TEST(TaskTest, DeleteTask_TaskBank)
+{
+
+}
+
+TEST(TaskTest, clearTaskBank)
+{
+
+}
+
+TEST(TaskTest, searchTaskBank)
+{
+
+}
+
+TEST(TaskTest, viewTask)
+{
+
+}
+
+TEST(TaskTest, TaskBankMemento)
+{
+
+}
