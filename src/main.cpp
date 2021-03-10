e823a66ec53876694d7dbed2aba4d7595c6efc11
galacticgoob <jameso1321@gmail.com>
2021-03-09 22:21:01 -0800

    Fixed compiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-/*
+
 #include "/home/csmajs/jooi001/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
 #include "/home/csmajs/jooi001/cs100-task-manager/header/Task.hpp"
 #include "/home/csmajs/jooi001/cs100-task-manager/header/TaskManager.hpp"
 #include "/home/csmajs/jooi001/cs100-task-manager/header/helperFunctions.hpp"
-*/
+/*
 
 #include "/home/csmajs/mchu017/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
 #include "/home/csmajs/mchu017/cs100-task-manager/header/Task.hpp"
@@ -11,7 +11,7 @@
 //#include "/home/csmajs/mchu017/cs100-task-manager/header/helperFunctions.hpp"
 //#include "/home/csmajs/mchu017/cs100-task-manager/header/TaskBankMemento.hpp"
 #include "/home/csmajs/mchu017/cs100-task-manager/header/TaskBankCommand.hpp"
-
+*/
 //#include "/home/csmajs/htran164/cs100-task-manager/header/TaskList.hpp" // change to your branch directory
 //#include "/home/csmajs/htran164/cs100-task-manager/header/Task.hpp"
 //#include "/home/csmajs/mchu017/cs100-task-manager/header/TaskManager.hpp"
